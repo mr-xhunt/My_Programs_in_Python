2482fa293870b9ced2bd8b3c8b4c39ef4a69ddb1
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-15 21:59:33 +0530

    Update prettify myfolder.py
    15/01/2022

--- prettify myfolder.py
+++ prettify myfolder.py
@@ -15,15 +15,15 @@ def soldier(path, file, format):
     i = 1
     files = os.listdir(path)
     with open(file) as f:
-        filelist = f.read().split("\n")
+        filelist = f.read().split("\n")           #here we read every file 
 
     for file in files:
         if file not in filelist:
-            os.rename(file, file.capitalize())
+            os.rename(file, file.capitalize())   #capitalizing the file which we read
 
         if os.path.splitext(file)[1] == format:
             os.rename(file, f"{i}{format}")
-            i +=1
+            i +=1                                   #here the formats gets arranged from no. 1 to n
 
 soldier(r"D:\Mayank\Projects mayank\prettyfolder",
         r"D:\Mayank\Projects mayank\hii.txt", ".jpg" )
