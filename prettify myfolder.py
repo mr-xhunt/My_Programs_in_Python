48d0bd9ee008663271870277284f53a015b40bf3
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-15 21:57:29 +0530

    solved

--- prettify myfolder.py
+++ prettify myfolder.py
@@ -7,3 +7,23 @@
 # this that normal
 
 # def soldier("C://", "harry.txt", "jpg")
+
+
+import os
+def soldier(path, file, format):
+    os.chdir(path)
+    i = 1
+    files = os.listdir(path)
+    with open(file) as f:
+        filelist = f.read().split("\n")
+
+    for file in files:
+        if file not in filelist:
+            os.rename(file, file.capitalize())
+
+        if os.path.splitext(file)[1] == format:
+            os.rename(file, f"{i}{format}")
+            i +=1
+
+soldier(r"D:\Mayank\Projects mayank\prettyfolder",
+        r"D:\Mayank\Projects mayank\hii.txt", ".jpg" )
