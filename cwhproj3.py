bf4a6c33ed3709c48028ca269f77acf3e3616e33
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-23 21:03:41 +0530

    cwh
    23/01/2022

---
+++ cwhproj3.py
@@ -0,0 +1,30 @@
+# make a program
+# take input of lists
+# output:
+# print three list : one will be reversed of the given input , second will be swaped by like first thing to last second to second last and so on,
+#  third by using one of a built in function
+
+
+food_calories=list(map(int,input("Enter The Calories:").split()))
+print(food_calories)
+
+l2=list(reversed(food_calories))
+l3=food_calories[::-1]
+
+length=len(food_calories)
+
+for i in range(length//2):
+    temp=food_calories[i]
+    food_calories[i]=food_calories[-1+-i]
+    food_calories[-i+-1]=temp
+print(l2)
+print(l2)
+print(food_calories)
+if(l2==l3==food_calories):
+    print("Same")
+
+
+
+
+
+
