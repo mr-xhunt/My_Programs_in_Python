d5bc3c567cf456e30ae3f3ff70a29945fd704ddb
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-30 15:45:58 +0530

    version 1.0

--- Mx_Stock_Screener.py
+++ Mx_Stock_Screener.py
@@ -1,76 +1,103 @@
 from nsetools import Nse
-import pandas as pd
-
-'''
-YOU MUST NEED TO DOWNLOAD NIFTY50 CSV FILE TO BEGIN WITH AND PLACE THE FILE IN SAME FOLDER AS YOU PLACE THIS PYTHON FILE
-
-LINK TO DOWNLOAD FILE: https://www1.nseindia.com/content/indices/ind_nifty50list.csv 
-
-NIFTY NEXT 50 : https://www1.nseindia.com/content/indices/ind_niftynext50list.csv 
-
-'''
 
 
-data = pd.read_csv("ind_nifty50list.csv")
-df = data.head(50)
-  
-nifty50 = df["Symbol"].tolist()
-
+nifty100 = ['ADANIPORTS', 'ASIANPAINT', 'AXISBANK', 'BAJAJ-AUTO', 'BAJFINANCE', 'BAJAJFINSV', 'BPCL', 'BHARTIARTL', 'BRITANNIA', 'CIPLA', 'COALINDIA', 'DIVISLAB', 'DRREDDY', 'EICHERMOT', 'GRASIM', 'HCLTECH', 'HDFCBANK', 'HDFCLIFE', 'HEROMOTOCO', 'HINDALCO', 'HINDUNILVR', 'HDFC', 'ICICIBANK', 'ITC', 'IOC', 'INDUSINDBK', 'INFY', 'JSWSTEEL', 'KOTAKBANK', 'LT', 'M&M', 'MARUTI', 'NTPC', 'NESTLEIND', 'ONGC', 'POWERGRID', 'RELIANCE', 'SBILIFE', 'SHREECEM', 'SBIN', 'SUNPHARMA', 'TCS', 'TATACONSUM', 'TATAMOTORS', 'TATASTEEL', 'TECHM', 'TITAN', 'UPL', 'ULTRACEMCO', 'WIPRO','ACC', 'ADANIENT', 'ADANIGREEN', 'ADANITRANS', 'AMBUJACEM', 'APOLLOHOSP', 'AUROPHARMA', 'DMART', 'BAJAJHLDNG', 'BANDHANBNK', 'BANKBARODA', 'BERGEPAINT', 'BIOCON', 'BOSCHLTD', 'CADILAHC', 'CHOLAFIN', 'COLPAL', 'DLF', 'DABUR', 'GAIL', 'GLAND', 'GODREJCP', 'HDFCAMC', 'HAVELLS', 'HINDPETRO', 'ICICIGI', 'ICICIPRULI', 'IGL', 'INDUSTOWER', 'NAUKRI', 'INDIGO', 'JINDALSTEL', 'JUBLFOOD', 'LTI', 'LUPIN', 'MARICO', 'MUTHOOTFIN', 'NMDC', 'PIIND', 'PIDILITIND', 'PEL', 'PGHH', 'PNB', 'SBICARD', 'SIEMENS', 'SAIL', 'TORNTPHARM', 'MCDOWELL-N', 'VEDL', 'YESBANK']
 
 nse = Nse()
 
-def mx_int_scan(scan_type, range):
-    if scan_type == "1":
-        print("Scanning................................................................................")
-        for company in nifty50:
-            quote = nse.get_quote(str(company))
-            company_name = str(quote['companyName'])
-            curr_price = int(quote['closePrice'])
-            highest_price = int(quote['high52'])
-            if curr_price > highest_price or curr_price == highest_price:
-                print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : Highest of All Time! \n")
-            else:
-                print("Sorry! No data found for your Request\n")
-                break
 
+def nifty50_scanner(choice,range):
+        if choice == '1':
+            for company in nifty100[0:50]:
+                quote = nse.get_quote(str(company))
+                company_name = str(quote['companyName'])
+                curr_price = int(quote['closePrice'])
+                highest_price = int(quote['high52'])
+                if range == "10":
+                    if curr_price < (highest_price - highest_price*0.1):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 10 Percent Lesser than High \n")
+                elif range == "20":
+                    if curr_price < (highest_price - highest_price*0.2):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 20 Percent Lesser than High\n")
+                elif range == "30":
+                    if curr_price < (highest_price - highest_price*0.3):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 30 Percent Lesser than High\n")
+                elif range == "50":
+                    if curr_price < (highest_price - highest_price*0.5):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 50 Percent Lesser than High\n")
+                else:
+                    print("Invalid Command")
+                    break
 
-    elif scan_type == "2":
-        print("Scanning................................................................................")
-        for company in nifty50:
-            quote = nse.get_quote(str(company))
-            company_name = str(quote['companyName'])
-            curr_price = int(quote['closePrice'])
-            highest_price = int(quote['high52'])
-            if range == "10":
-                if curr_price < (highest_price - highest_price*0.1):
-                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 10 Percent Lesser than High \n")
-            elif range == "20":
-                if curr_price < (highest_price - highest_price*0.2):
-                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 20 Percent Lesser than High\n")
-            elif range == "30":
-                if curr_price < (highest_price - highest_price*0.3):
-                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 30 Percent Lesser than High\n")
-            elif range == "50":
-                if curr_price < (highest_price - highest_price*0.5):
-                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 50 Percent Lesser than High\n")
-            else:
-                print("Invalid Command")
-    else:
-        print("Invalid Command")
+        elif choice == '2':
+            print("Scanning................................................................................")
+            for company in nifty100[0:50]:
+                quote = nse.get_quote(str(company))
+                company_name = str(quote['companyName'])
+                curr_price = int(quote['closePrice'])
+                highest_price = int(quote['high52'])
+                if curr_price > highest_price or curr_price == highest_price:
+                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : Highest of All Time! \n")
+                else:
+                    print("Sorry! No data found for your Request\n")
+                    break
 
-
+def nifty100_scanner(choice,range):
+        if choice == '1':
+            for company in nifty100:
+                quote = nse.get_quote(str(company))
+                company_name = str(quote['companyName'])
+                curr_price = int(quote['closePrice'])
+                highest_price = int(quote['high52'])
+                if range == "10":
+                    if curr_price < (highest_price - highest_price*0.1):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 10 Percent Lesser than High \n")
+                elif range == "20":
+                    if curr_price < (highest_price - highest_price*0.2):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 20 Percent Lesser than High\n")
+                elif range == "30":
+                    if curr_price < (highest_price - highest_price*0.3):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 30 Percent Lesser than High\n")
+                elif range == "50":
+                    if curr_price < (highest_price - highest_price*0.5):
+                        print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : 50 Percent Lesser than High\n")
+                else:
+                    print("Invalid Command")
+                    break
+                
+        elif choice == '2':
+            print("Scanning................................................................................")
+            for company in nifty100:
+                quote = nse.get_quote(str(company))
+                company_name = str(quote['companyName'])
+                curr_price = int(quote['closePrice'])
+                highest_price = int(quote['high52'])
+                if curr_price > highest_price or curr_price == highest_price:
+                    print(f"{company_name} : Current Price = {curr_price} : High52 = {highest_price} : Highest of All Time! \n")
+                else:
+                    print("Sorry! No data found for your Request\n")
+                    break
 
 while True:
-    inp = input("What type of scan you want to perform!: \n1. Upward Scan \n 2. Lower Scan\n Type 'q' to quit or '-h' for help:  ")
-    if inp == "q":
+    print("\nThis Scanner is developed by:- Mayank Kaushik©\n")
+    print("Type '1' or '2' as per your Choice!   or type 'q' to quit...")
+    userinp = input("Do You Want to Scan Nifty50 or Nifty100:  ")
+    if userinp == 'q':
         break
-    elif inp == "-h" or inp == 'help':
-        print("Upward Scan: Scan Stocks which is at all time high\n Lower Scan: Scan Stocks Which are lower than all time high\n You can use 10, 20,30,50 to view Percentange difference.")
-    elif inp == "1":
-        mx_int_scan(inp,range)
-    elif inp == "2":
-        range = input("Type 10, 20, 30 or 50 to view Percentage Difference!\nEnter the Percent Difference to Scan: ")
-        mx_int_scan(inp,range)
+    elif userinp == '1':
+        choice = input("Scan Type: \n1. Value below 52week high\n 2. All time high/52 week high\nWhat Scan do you wish to perform: ")
+        if choice == '2':
+            nifty50_scanner(choice, range)
+        else:
+            range = input("Input range to see Percentage difference: 10, 20, 30 or 50: ")
+            nifty50_scanner(choice, range)
+
+    elif userinp == '2':
+        choice = input("Scan Type: \n1. Value below 52week high\n 2. All time high/52 week high\nWhat Scan do you wish to perform: ")
+        if choice == '2':
+            nifty50_scanner(choice, range)
+        else:
+            range = input("Input range to see Percentage difference: 10, 20, 30 or 50: ")
+            nifty100_scanner(choice, range)
     else:
-        print("Invalid")
         break
