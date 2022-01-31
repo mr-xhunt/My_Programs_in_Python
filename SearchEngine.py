1f342b3c0c1203a686c3d2895518fd4f78dcb935
Mayank Kumar Choubey <61092858+mr-xhunt@users.noreply.github.com>
2022-01-31 19:58:12 +0530

    UnderConstruction

---
+++ SearchEngine.py
@@ -0,0 +1,12 @@
+"""Make a search engine 
+list of strings is already given to you
+take input from user what word or sentence they are searching for
+diplay user their desired result in decreasing order of relevence
+
+eg. lists= ['mayank is a very good boy', 'this is a very good work and good result', 'this is all about python']
+search query = good
+output = 
+2 search result in 0.009 second 
+this is a very good work and good result
+mayank is a very good boy
+"""
